a5aa0b35ee615890f16db5051bded4fc383f28ed
Amanda Tree <amandasimonds9@gmail.com>
2019-11-13 18:31:58 -0700

    prompt user started

--- app.js
+++ app.js
@@ -1,44 +1,47 @@
-class Employee {
-    constructor(name, id, title) {
-      if (!name) {
-        throw new Error("You are missing the name.");
-      }
-      if (!id) {
-        throw new Error("You are missing the ID.");
-      }
-      if (!title) {
-        throw new Error("You are missing the title.");
-      }
-      this.name = name;
-      this.id = id;
-      this.title = title;
-    }
-  
-    // method which prints all of the stats for a character
-    printInfo() {
-      console.log(`Name: ${this.name}`);
-      console.log(`ID: ${this.id}`);
-      console.log(`title: ${this.title}`);
-    }
-  
-    getName() {
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-    }
+function promptUser(){
+    return inquirer.prompt([
+        
+          {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+          },
 
-    getId(){
+          {
+            type: "list",
+            name: "role",
+            message: "What is your role?",
+            choices: ["Manager", "Intern", "Engineer"]
+          },
 
-    }
+          
 
-    getEmail(){
+       ]).then(function(role){
 
-    }
+        if (role === "Engineer"){
+            return inquirer.prompt([
+                {
+                    type: "input",
+                    name: "github",
+                    message: "What is your github link?",
+                }
+            ])
+        } else if (role == "intern"){
+            return inquirer.prompt([
+                {
+                    type: "input",
+                    name: "school",
+                    message: "Enter the school you are currently enrolled in:"
+                }
+            ])
+        } else {
+            console.log("no role found")
+        }
 
-    getRole(){
-        return Employee;
-    }
-  }
-  
-  // creates two unique characters using the "character" constructor
-  const grace = new Character("Grace", 30, 75);
-  
-  grace.printInfo();
+        console.log(this.role)
+       })};
+
+       promptUser();
